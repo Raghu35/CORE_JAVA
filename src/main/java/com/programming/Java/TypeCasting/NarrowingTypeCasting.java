c8879cb92e10297e01a5e67f9ba8d92feffdeb03
Raghu AS <raghu.as98@gmail.com>
2022-08-04 11:26:06 +0530

    Added Casting Classes

--- src/main/java/com/programming/Java/TypeCasting/NarrowingTypeCasting.java
+++ src/main/java/com/programming/Java/TypeCasting/NarrowingTypeCasting.java
@@ -14,5 +14,26 @@ public class NarrowingTypeCasting {
         //jhgf
 //fractional part lost
         System.out.println("After conversion into int type: "+i);
-    }
+
+         float input = 65.0f ;
+         byte b = (byte) input ;
+         short s = (short) input ;
+         char c = (char) input ;
+         int e = (int) input ;
+         System.out.println("Examples of Narrowing primitive Type casting...!!");
+         System.out.println("float to short : "+b);
+         System.out.println("float to byte : "+s);
+         System.out.println("float to char : "+c);
+         System.out.println("float to int : "+e);
+
+         System.out.println("------------------------------------------------------------------------------------");
+         //IMPORTANT
+         int q = 70;
+         float w = (float)q;
+         System.out.println("int to float : "+w);
+         char z = (char) w ;
+         System.out.println("float to char : "+z);
+         int v = (int)z;
+         System.out.println("char to int : "+v);
+     }
 }
